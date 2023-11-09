1b85bf32bf007db544c795971962df1fba56bdcb
Joël de Bont <joel@bluenotion.nl>
2023-11-09 12:42:38 +0100

    added metadata

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,7 +2,12 @@ import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
 import { notFound, redirect } from 'next/navigation';
+import { Metadata } from 'next';
  
+export const metadata: Metadata = {
+  title: 'Edit Invoice',
+};
+
 export default async function Edit({ params }: { params: { id: string } }) {
     const id = params.id;
     const [invoice, customers] = await Promise.all([
